6f59fea611e32c7f86fdf7ae404ff13819174bfe
Your Name <you@example.com>
2022-07-02 16:50:05 +0530

    checking a day is valid or invaid is implemented

--- src/day2/Day_2.java
+++ src/day2/Day_2.java
@@ -6,14 +6,18 @@ public class Day_2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the value of a, b and c to find the roots of the equation");
+		System.out.println("Enter the value of a, b month and day respectively");
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		int c = sc.nextInt();
-		float root1 = (float) (-b + Math.sqrt(Math.pow(b, 2))-4*a*c) / 2*a;
-		float root2 = (float) (-b - Math.sqrt(Math.pow(b, 2))-4*a*c) / 2*a;
-		System.out.println("The roots of the quadratic equation "+a+"^2+"+b+"x+"+c+" = "+root1+","+root2);
-		sc.close();
+		if(a>=3 && a<=6) {
+			if((a==3 && b>20) ||(a==6 && b<20) || (a>3 && a<6)) {
+				System.out.println("valid");
+			}
+			else {
+				System.out.println("invalid");
+			}
+		}
+	sc.close();
 	}
 
 }
