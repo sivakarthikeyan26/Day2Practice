ecbf1e8816f6f0f048882cbe3d90fb922cae5b1e
Your Name <you@example.com>
2022-07-02 17:04:04 +0530

    operator operation

--- src/day2/Day_2.java
+++ src/day2/Day_2.java
@@ -6,18 +6,21 @@ public class Day_2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the value of a, b month and day respectively");
+		System.out.println("Enter the value of a, b and c");
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		if(a>=3 && a<=6) {
-			if((a==3 && b>20) ||(a==6 && b<20) || (a>3 && a<6)) {
-				System.out.println("valid");
+		int c = sc.nextInt();
+		if(a>b) {
+			System.out.println("a is greatest");
 			}
-			else {
-				System.out.println("invalid");
-			}
+		else
+			System.out.println("b is greater");
+		
+		System.out.println("The output of a+b*c = "+(a+b+c));
+		System.out.println("The output of c+a/b = "+(c+a/b));
+		System.out.println("The output of a%b+c = "+(a%b+c));
+		System.out.println("The output of a*b+c = "+(a*b+c));
+		sc.close();
 		}
-	sc.close();
 	}
 
-}
