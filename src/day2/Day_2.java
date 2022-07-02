ee563a50dab2ce3f5934164e2ef902ea80415d25
Your Name <you@example.com>
2022-07-02 17:08:53 +0530

    vowel or consonant is implemented

--- src/day2/Day_2.java
+++ src/day2/Day_2.java
@@ -6,21 +6,57 @@ public class Day_2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the value of a, b and c");
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		if(a>b) {
-			System.out.println("a is greatest");
-			}
-		else
-			System.out.println("b is greater");
+		System.out.println("Enter the Alphabet for check vowel or consonant  ");
+		char ch=sc.next().charAt(0);
+		switch(ch){
+		    //check lower case vowel letters
+		    case 'a':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'e':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'i':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'o':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'u':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    //check upper case vowel letters
+		    case 'A':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'E':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'I':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'O':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    case 'U':
+		    System.out.println(ch+" is a vowel");
+		    break;
+		    
+		    default:
+		    System.out.println(ch+" is a consonant");
+		    break;
 		
-		System.out.println("The output of a+b*c = "+(a+b+c));
-		System.out.println("The output of c+a/b = "+(c+a/b));
-		System.out.println("The output of a%b+c = "+(a%b+c));
-		System.out.println("The output of a*b+c = "+(a*b+c));
-		sc.close();
 		}
+		sc.close();
 	}
+}
 
