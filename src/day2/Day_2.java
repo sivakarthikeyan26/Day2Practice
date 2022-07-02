49da4ba0f1f5bfa40026151bce95a72447a76008
Your Name <you@example.com>
2022-07-02 17:20:10 +0530

    Program to print the month name for the given number

--- src/day2/Day_2.java
+++ src/day2/Day_2.java
@@ -6,56 +6,49 @@ public class Day_2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Alphabet for check vowel or consonant  ");
-		char ch=sc.next().charAt(0);
-		switch(ch){
-		    //check lower case vowel letters
-		    case 'a':
-		    System.out.println(ch+" is a vowel");
-		    break;
-		    
-		    case 'e':
-		    System.out.println(ch+" is a vowel");
-		    break;
-		    
-		    case 'i':
-		    System.out.println(ch+" is a vowel");
-		    break;
-		    
-		    case 'o':
-		    System.out.println(ch+" is a vowel");
-		    break;
-		    
-		    case 'u':
-		    System.out.println(ch+" is a vowel");
-		    break;
-		    
-		    //check upper case vowel letters
-		    case 'A':
-		    System.out.println(ch+" is a vowel");
-		    break;
-		    
-		    case 'E':
-		    System.out.println(ch+" is a vowel");
-		    break;
-		    
-		    case 'I':
-		    System.out.println(ch+" is a vowel");
-		    break;
-		    
-		    case 'O':
-		    System.out.println(ch+" is a vowel");
-		    break;
-		    
-		    case 'U':
-		    System.out.println(ch+" is a vowel");
-		    break;
-		    
-		    default:
-		    System.out.println(ch+" is a consonant");
-		    break;
-		
-		}
+		 System.out.print("Enter month=");
+	       int month = sc.nextInt();
+	        switch (month)
+	        {
+	            case 1:
+	                System.out.println("January");
+	                break;
+	            case 2:
+	                System.out.println("February");
+	                break;
+	            case 3:
+	                System.out.println("March");
+	                break;
+	            case 4:
+	                System.out.println("April");
+	                break;
+	            case 5:
+	                System.out.println("May");
+	                break;
+	            case 6:
+	                System.out.println("June");
+	                break;
+	            case 7:
+	                System.out.println("July");
+	                break;
+	            case 8:
+	                System.out.println("August");
+	                break;
+	            case 9:
+	                System.out.println("September");
+	                break;
+	            case 10:
+	                System.out.println("October");
+	                break;
+	            case 11:
+	                System.out.println("November");
+	                break;
+	            case 12:
+	                System.out.println("December");
+	                break;
+	            default:
+	                System.out.println("Enter valid month.");
+	        }
 		sc.close();
 	}
 }
